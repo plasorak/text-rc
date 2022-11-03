c3ed733a90c52d521f58a1b9741fef49fc72df09
Pierre Lasorak <pierre.lasorak@gmail.com>
2022-11-03 17:48:27 +0000

    a better version of everything

--- rc.py
+++ rc.py
@@ -1,84 +1,237 @@
-
+import asyncio
+import logging
+logging.basicConfig(level=logging.INFO)
+import queue
 
 class RC:
 
-    def __init__(self, timeout:int=10):
+    def __init__(self, timeout:int=3):
         self.timeout = timeout # s
-        import logging
         self.log = logging.getLogger("RC")
+        # log_handle = logging.FileHandler("rc.log")
+        # self.log.addHandler(log_handle)
+
         self.state = 'none'
-        self.tree = {
-            'whatever': {
-                'whatever': None
+        self.none_state_tree = {
+            'np04_coldbox': {
+                'state': 'none',
+                'children': [
+                    {
+                        'wibs': {
+                            'state': 'none',
+                            'children': []
+                        }
+                    },
+                    {
+                        'daq': {
+                            'state': 'none',
+                            'children': []
+                        }
+                    },
+                    
+                ]
             }
         } # type: dict[str, dict]
+        self.tree = self.none_state_tree
 
     def get_available_commands(self) -> list[str]:
-        if self.state == 'none':
-            return ['start']
-        return ['stop']
+        if   self.state == 'none':                    return ['boot']
+        elif self.state == 'initialised':             return ['start_run', 'conf', 'terminate', 'shutdown']
+        elif self.state == 'configured':              return ['start_run', 'scrap', 'shutdown', 'start']
+        elif self.state == 'ready':                   return ['enable_trigger', 'drain_dataflow', 'shutdown']
+        elif self.state == 'trigger_enabled':         return ['disable_trigger', 'shutdown']
+        elif self.state == 'dataflow_drained':        return ['stop_trigger_sources', 'shutdown']
+        elif self.state == 'trigger_sources_stopped': return ['stop', 'shutdown']
+        else: return []
+
+    def get_all_commands(self) -> list[str]:
+        return [
+            'boot',
+            'start_run',
+            'conf',
+            'terminate',
+            'shutdown',
+            'scrap',
+            'start',
+            'enable_trigger',
+            'disable_trigger',
+            'drain_dataflow',
+            'stop_trigger_sources',
+            'stop',
+        ]
+
+    
+    def update_app_status(self,out_state) -> None:
+        self.tree = {
+            'np04_coldbox': {
+                'state': out_state,
+                'children': [
+                    {
+                        'wibs': {
+                            'state': out_state,
+                            'children': [
+                                {
+                                    f'wib_00{i}': {
+                                        'state': out_state
+                                    }
+                                    for i in range(8)
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        'daq': {
+                            'state': out_state,
+                            'children': [
+                                {
+                                    f'runp04srv0{i}': {
+                                        'state': out_state
+                                    }
+                                    for i in range(24,28)
+                                },
+                                {
+                                    f'dqmrunp04srv0{i}': {
+                                        'state': out_state
+                                    }
+                                    for i in range(24,28)
+                                },
+                                {
+                                    'dfo': {
+                                        'state': out_state
+                                    }
+                                },
+                                {
+                                    'trigger': {
+                                        'state': out_state
+                                    }
+                                },
+                                {
+                                    f'dataflow{i}': {
+                                        'state': out_state
+                                    }
+                                    for i in range(4)
+                                },
+                                {
+                                    f'dqmdf{i}': {
+                                        'state': out_state
+                                    }
+                                    for i in range(4)
+                                }
+                            ]
+                        }
+                    },
+                    
+                ]
+            }
+        } # type: dict[str, dict]
         
 
-    def send_command(self, command:str) -> None:
+        
+    async def send_command(self, command:str, in_state:str, out_state:str) -> None:
         import time
         from rich.progress import track
+        
+        if self.state != in_state:
+            raise RuntimeError(f'Cannot send {command} from \'{self.state}\'')
+        
+        self.state = command+'ing'
+        
         self.log.info(f'Preparing to send \'{command}\'')
-        for i in track(range(self.timeout), description=f"Sending {command}..."):
-            time.sleep(1)  # Simulate work being done
+        for i in track(range(self.timeout*10), description=f"Sending {command}..."):
+            self.log.info(f'Plenty of logs for the command \'{command}\'...')
+            await asyncio.sleep(0.1)  # Simulate work being done
+
+        if command == 'terminate':
+            self.tree = self.none_state_tree
+        else:
+            self.update_app_status(out_state)
+        
+        self.state = out_state
         self.log.info(f'Sent \'{command}\'')
 
 
-    def start(self) -> None:
-        if self.state != 'none':
-            raise RuntimeError('Cannot send start from \'started\'')
-        self.send_command('start')
-        self.state = 'started'
-        self.tree = {
-            'whatever': {
-                'whatever1': {
-                    "child1" : None,
-                    "child2" : None,
-                },
-                'whatever2': {
-                    "child1" : None,
-                    "child2" : None,
-                }
-            }
-        }
+    async def boot(self) -> None:
+        await self.send_command('boot', 'none', 'initialised')
+        
+    async def conf(self) -> None:
+        await self.send_command('conf', 'initialised', 'configured')
+
+    async def start(self) -> None:
+        await self.send_command('start', 'configured', 'ready')
+        
+    async def enable_trigger(self) -> None:
+        await self.send_command('enable_trigger', 'ready', 'trigger_enabled')
+
+    async def disable_trigger(self) -> None:
+        await self.send_command('disable_trigger', 'trigger_enabled', 'ready')
+
+    async def drain_dataflow(self) -> None:
+        await self.send_command('drain_dataflow', 'ready', 'dataflow_drained')
+
+    async def stop_trigger_sources(self) -> None:
+        await self.send_command('stop_trigger_sources', 'dataflow_drained', 'trigger_sources_stopped')
+    
+    async def stop(self) -> None:
+        await self.send_command('stop', 'trigger_sources_stopped', 'configured')
+        
+    async def scrap(self) -> None:
+        await self.send_command('scrap', 'configured', 'initialised')
+        
+    async def terminate(self) -> None:
+        await self.send_command('terminate', 'initialised', 'none')
 
 
-    def stop(self) -> None:
-        if self.state != 'started':
-            raise RuntimeError('Cannot send start from \'none\'')
-        self.send_command('stop')
-        self.state = 'stopped'
-        self.tree = {
-            'whatever': {
-                'whatever': None
-            }
-        }
-
+    async def execute_maybe(self, cmd, in_state, out_state):
+        try:
+            await self.send_command(cmd, in_state, out_state)
+        except:
+            pass
+            
+    async def shutdown(self) -> None:
+        await self.execute_maybe('disable_trigger',      'trigger_enabled',         'ready'                  )
+        await self.execute_maybe('drain_dataflow',       'ready',                   'dataflow_drained'       )
+        await self.execute_maybe('stop_trigger_sources', 'dataflow_drained',        'trigger_sources_stopped')
+        await self.execute_maybe('stop',                 'trigger_sources_stopped', 'configured'             )
+        await self.execute_maybe('scrap',                'configured',              'initialised'            )
+        await self.execute_maybe('terminate',            'initialised',             'none'                   )
+        
+    async def start_run(self) -> None:
+        await self.execute_maybe('conf',           'initialised', 'configured'     )
+        await self.execute_maybe('start',          'configured',  'ready'          )
+        await self.execute_maybe('enable_trigger', 'ready',       'trigger_enabled')
+            
+        
         
 
-if __name__ == '__main__':
+
+async def main():
     from rich.logging import RichHandler
     import logging
-    
-    logging.basicConfig(
-        level="INFO",
-        format="%(filename)s:%(lineno)d %(message)s",
-        datefmt="[%X]",
-        handlers=[RichHandler(rich_tracebacks=True)]
-    )
-    
-    log = logging.getLogger("main")
+
+    # logging.basicConfig(
+    #     level="INFO",
+    #     format="%(filename)s:%(lineno)d %(message)s",
+    #     datefmt="[%X]",
+    #     handlers=[RichHandler(rich_tracebacks=True)]
+    # )
+
+    # log = logging.getLogger("main")
     rc = RC(timeout=4)
-    log.info(f'RC state: \'{rc.state}\', available commands: {rc.get_available_commands()}')
+    log_queue = queue.Queue(-1)
+    queue_handler = QueueHandler(queue)
+    log.addHandler(QueueHandler(queue))
+
+    
+    # log.info(f'RC state: \'{rc.state}\', available commands: {rc.get_available_commands()}')
     import json
-    log.info(f'RC tree: {json.dumps(rc.tree, indent=2)}')
-    rc.start()
-    log.info(f'RC state: \'{rc.state}\', available commands: {rc.get_available_commands()}')
-    log.info(f'RC tree: {json.dumps(rc.tree, indent=2)}')
-    rc.stop()
-    log.info(f'RC state: \'{rc.state}\', available commands: {rc.get_available_commands()}')
-    log.info(f'RC tree: {json.dumps(rc.tree, indent=2)}')
+    # log.info(f'RC tree: {json.dumps(rc.tree, indent=2)}')
+    await rc.start()
+    # log.info(f'RC state: \'{rc.state}\', available commands: {rc.get_available_commands()}')
+    # log.info(f'RC tree: {json.dumps(rc.tree, indent=2)}')
+    await rc.stop()
+    # log.info(f'RC state: \'{rc.state}\', available commands: {rc.get_available_commands()}')
+    # log.info(f'RC tree: {json.dumps(rc.tree, indent=2)}')
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
--- tui.py
+++ tui.py
@@ -1,86 +1,152 @@
+import asyncio
 from rc import RC
 from textual import log
+from rich.logging import RichHandler
+from rich.text import Text
+from rich.json import JSON
+from rich.markdown import Markdown
 from textual.app import App, ComposeResult
-from textual.containers import Container, Content
+from textual.containers import Horizontal, Content, Container, Vertical
+
 from textual.widgets import Button, Header, Footer, Static
 from textual.reactive import reactive
+import logging
+from logging.handlers import QueueHandler, QueueListener
+import queue
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class TitleBox(Static):
+    def __init__(self, title, **kwargs):
+        super().__init__(Markdown(f'# {title}'))
 
 class LogDisplay(Static):
-    pass
+    logs = reactive('')
 
-class LogBox(Static):
-    def __init__(self, rc, **kwargs):
+    def __init__(self, log_queue, **kwargs):
         super().__init__(**kwargs)
-        self.rc = rc
+        self.log_queue = log_queue
+        self.handler = RichHandler()
+    
+    def on_mount(self) -> None:
+        self.set_interval(0.1, self.update_logs) # execute update_logs every second
+    
+    def update_logs(self) -> None:
+        while True: # drain the queue of logs
+            try:
+                record = self.log_queue.get(block=False)
+                text = self.handler.render_message(record, record.msg)
+                self.logs += f'{text}\n'
+            except:
+                break
 
+    def watch_logs(self, logs:str) ->None:
+        self.update(logs)
+        
+class Logs(Static):
+    def __init__(self, log_queue, **kwargs):
+        super().__init__(**kwargs)
+        self.log_queue = log_queue
+    
+    def compose(self) -> ComposeResult:
+        yield TitleBox('Logs')
+        yield Vertical(LogDisplay(self.log_queue), id='verticallogs')
+        
 
-class StatusDisplay(Static):
-    pass
-
+class StatusDisplay(Static): pass
 
 class Status(Static):
-    rc = reactive(RC)
-    
+    rcstatus = reactive('none')
+
     def __init__(self, rc, **kwargs):
         super().__init__(**kwargs)
         self.rcobj = rc
 
-    def update_status(self) -> None:
+    def update_rcstatus(self) -> None:
+        self.rcstatus = self.rcobj.state
+
+    def watch_rcstatus(self, status:str) -> None:
         status_display = self.query_one(StatusDisplay)
-        status_display.update(self.rcobj.state)
-        
-    def watch_rcobj(self) -> None:
-        self.update_status()
-        
+        status_display.update(Markdown(f'# Status: {status}'))
+
     def on_mount(self) -> None:
-        self.update_status()
+        self.set_interval(0.1, self.update_rcstatus)
 
     def compose(self) -> ComposeResult:
+        # yield TitleBox("Status {}")
         yield StatusDisplay()
-        yield Button('Update status', id='update_status', variant='primary')
 
-    def on_button_pressed(self, event: Button.Pressed) -> None:
-        """Event handler called when a button is pressed."""
-        if event.button.id == "update_status":
-            self.update_status()
+class TreeDisplay(Static): pass
 
 class TreeView(Static):
+    rctree = reactive('')
+    
     def __init__(self, rc, **kwargs):
         super().__init__(**kwargs)
-        self.rc = rc
+        self.rcobj = rc
+        
+    def compose(self) -> ComposeResult:
+        yield TitleBox("Apps")
+        yield Vertical(TreeDisplay(), id='verticaltree')
+    
+    def update_rctree(self) -> None:
+        self.rctree = self.rcobj.tree
 
-    def update_tree(self) -> None:
+    def watch_rctree(self, tree:dict) -> None:
+        tree_display = self.query_one(TreeDisplay)
         import json
-        self.update(json.dumps(self.rc.tree, indent=2))
+        tree_display.update(JSON(json.dumps(tree)))
 
     def on_mount(self) -> None:
-        self.update_tree()
+        self.set_interval(0.1, self.update_rctree)
 
-class StateBox(Static):
+class Command(Static):
+    commands = reactive([])
+    
     def __init__(self, rc, **kwargs):
         super().__init__(**kwargs)
-        self.rc = rc
-    
-    def compose(self) -> ComposeResult:
-        #Creates the initial set of buttons
-
-        for c in self.rc.get_available_commands():
-            yield Button(c, id=c, variant="primary")
+        self.rcobj = rc
         
-    
-    def on_button_pressed(self, event: Button.Pressed) -> None:
-        """Event handler called when a button is pressed."""
-        button_id = event.button.id
-        method = getattr(self.rc, button_id)    #We use the name of the button to find the right method of the Nanorc class
-        method()
+    def on_mount(self) -> None:
+        self.set_interval(0.1, self.update_buttons)
 
-class TUIApp(App):
+    def update_buttons(self) -> None:
+        self.commands = self.rcobj.get_available_commands()
+        
+    def watch_commands(self, commands:list[str]) -> None:
+        # for cmd self.rcobj.get_all_commands():
+        for button in self.query(Button):
+            if button.id in self.commands:
+                button.display=True
+            else:
+                button.display=False
+        
+    def compose(self) -> ComposeResult:
+        yield TitleBox('Commands')
+        yield Horizontal(
+            *[Button(b, id=b) for b in self.rcobj.get_all_commands()],
+            classes='buttonscontainer',
+        )
+
+    async def on_button_pressed(self, event: Button.Pressed) -> None:
+         """Event handler called when a button is pressed."""
+         button_id = event.button.id
+         method = getattr(self.rcobj, button_id) # We use the name of the button to find the right method of the Nanorc class
+         task = asyncio.create_task(method())
+
+
+class NanoRCTUI(App):
     CSS_PATH = "tui.css"
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
 
     def __init__(self, rc, **kwargs):
         super().__init__(**kwargs)
         self.rc = rc
+        self.log_queue = queue.Queue(-1)
+        self.queue_handler = QueueHandler(self.log_queue)
+        self.rc.log.propagate = False
+        self.rc.log.addHandler(self.queue_handler)
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
@@ -88,15 +154,19 @@ class TUIApp(App):
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
-        yield TreeView(rc=self.rc)
-        yield Status(rc=self.rc)
-        yield StateBox(rc=self.rc)
-        yield LogBox(rc=self.rc)
-        yield Header()
-        yield Footer()
+        yield Container(
+            Status  (rc = self.rc, classes='container'),
+            Command (rc = self.rc, classes='container', id='command'),
+            TreeView(rc = self.rc, classes='container', id='tree'),
+            Logs    (log_queue=self.log_queue, classes='container', id='log'),
+            id = 'app-grid'
+        )
         
+        yield Header(show_clock=True)
+        yield Footer()
+
 
 if __name__ == "__main__":
     rc = RC()
-    app = TUIApp(rc)
+    app = NanoRCTUI(rc)
     app.run()
